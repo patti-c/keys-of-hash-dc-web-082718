c6ac926db40f8a17330a50810996065103df0faf
Patricia Carter <patricia4194@gmail.com>
2018-08-17 15:24:45 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,5 @@
 animals = { 
-  {"sugar glider"=>"Australia",
-    "aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
+  {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} 
 }
 
 
