1a54c5e3fae6a664c19b99a4c946baa09826f36c
Patricia Carter <patricia4194@gmail.com>
2018-08-17 15:23:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,3 +1,6 @@
+:animals = { {"sugar glider"=>"Australia","aye-aye"=> "Madagascar","red-footed tortoise"=>"Panama","kangaroo"=> "Australia","tomato frog"=>"Madagascar","koala"=>"Australia"} }
+
+
 class Hash
   def keys_of(*args)
     array = []
